d2b00e36b89bfa21fede784006c3ffb0228f1ffd
Hakob Ghlijyan <hakobghlijyan@gmail.com>
2024-05-23 18:43:28 +0400

    [Features] Add first logic action for game, is tap gesture

--- TicTacToeSwiftUIGame/ContentView.swift
+++ TicTacToeSwiftUIGame/ContentView.swift
@@ -9,13 +9,18 @@ import SwiftUI
 
 struct ContentView: View {
     let columns = Array(repeating: GridItem(.flexible(), spacing: 4), count: 3)
+    // This My empty 9 places, start game
+    @State private var moves: [Move?] = Array(repeating: nil, count: 9)
+    @State private var isHumanTurn = true
     
     var body: some View {
         GeometryReader { geometry in
             VStack(spacing: 0) {
                 Spacer()
                 LazyVGrid(columns: columns, spacing: 4) {
-                    ForEach(0..<9) { item in
+                    
+                    ForEach(0..<9) { i in
+                        
                         ZStack {
                             Circle()
                                 .foregroundStyle(.red).opacity(0.8)
@@ -23,20 +28,36 @@ struct ContentView: View {
                                     width: (geometry.size.width/3) - 4,
                                     height: (geometry.size.width/3 - 4)
                                 )
-//                                .background(.black)
                             
-                            Image(systemName: "xmark")
+                            Image(systemName: moves[i]?.indicator ?? "")
                                 .resizable()
                                 .frame(width: 40, height: 40)
                                 .foregroundStyle(.white)
                         }
+                        .onTapGesture {
+                            moves[i] = Move(player: isHumanTurn ? .human : .computer, boardIndex: i)
+                            isHumanTurn.toggle()
+                        }
+                        
                     }
                 }
                 Spacer()
             }
-//            .background(.blue)
         }
-        .padding(4)
+        .padding(8)
+    }
+}
+
+enum Player {
+    case human, computer
+}
+
+struct Move {
+    let player: Player
+    let boardIndex: Int
+    
+    var indicator: String {
+        return player == .human ? "xmark" : "circle"
     }
 }
 
