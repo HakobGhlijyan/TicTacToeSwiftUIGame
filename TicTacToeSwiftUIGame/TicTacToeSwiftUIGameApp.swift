f76989f49b196012fd694980b393ade083b5b259
Hakob Ghlijyan <hakobghlijyan@gmail.com>
2024-05-23 17:12:19 +0400

    Initial Commit

---
+++ TicTacToeSwiftUIGame/TicTacToeSwiftUIGameApp.swift
@@ -0,0 +1,17 @@
+//
+//  TicTacToeSwiftUIGameApp.swift
+//  TicTacToeSwiftUIGame
+//
+//  Created by Hakob Ghlijyan on 23.05.2024.
+//
+
+import SwiftUI
+
+@main
+struct TicTacToeSwiftUIGameApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
