cc5bcb9dec70ec28607d9a00a288bad772177a36
Hakob Ghlijyan <hakobghlijyan@gmail.com>
2024-05-23 18:20:24 +0400

    [Feature] Create UI Screen

--- TicTacToeSwiftUIGame/ContentView.swift
+++ TicTacToeSwiftUIGame/ContentView.swift
@@ -8,14 +8,35 @@
 import SwiftUI
 
 struct ContentView: View {
+    let columns = Array(repeating: GridItem(.flexible(), spacing: 4), count: 3)
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        GeometryReader { geometry in
+            VStack(spacing: 0) {
+                Spacer()
+                LazyVGrid(columns: columns, spacing: 4) {
+                    ForEach(0..<9) { item in
+                        ZStack {
+                            Circle()
+                                .foregroundStyle(.red).opacity(0.8)
+                                .frame(
+                                    width: (geometry.size.width/3) - 4,
+                                    height: (geometry.size.width/3 - 4)
+                                )
+//                                .background(.black)
+                            
+                            Image(systemName: "xmark")
+                                .resizable()
+                                .frame(width: 40, height: 40)
+                                .foregroundStyle(.white)
+                        }
+                    }
+                }
+                Spacer()
+            }
+//            .background(.blue)
         }
-        .padding()
+        .padding(4)
     }
 }
 
